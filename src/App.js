1573a3e452ab67f9ad10d285361b2cb0666e9c86
John Doe <k3655692@gmail.com>
2024-04-03 14:54:35 +0300

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './component/header/header';
 import Main from './component/main/main';
 import Menu from './component/menu/menu';
 import React from 'react';
+import { YMInitializer } from 'react-yandex-metrika'; // Импорт YMInitializer
 
 function App() {
-
   return (
     <div className="App">
       <Header />
-      <Menu/>
+      <Menu />
       <Main />
+      {/* Инициализация Яндекс.Метрики с вашим ID счетчика */}
+      <YMInitializer accounts={[96923782]} options={{ webvisor: true }} />
     </div>
   );
 }
