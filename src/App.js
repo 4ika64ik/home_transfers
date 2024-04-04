8c60dd61cb8c69ee79f147363ff3867fa7ef630d
Max Petrashishin <rikogjdefolt@gmail.com>
2024-04-04 11:30:35 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './component/footer/footer';
 import Header from './component/header/header';
 import Main from './component/main/main';
 import Menu from './component/menu/menu';
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Menu />
       <Main />
+      <Footer />
       {/* Инициализация Яндекс.Метрики с вашим ID счетчика */}
       <YMInitializer accounts={[96923782]} options={{ webvisor: true }} />
     </div>
