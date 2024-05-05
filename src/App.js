879e31a4466527493cf2af7fb6597e7e892e592c
John Doe <k3655692@gmail.com>
2024-05-05 22:17:04 +0300

    rezt_px

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import Footer from './component/footer/footer';
 import Header from './component/header/header';
 import Main from './component/main/main';
 import Menu from './component/menu/menu';
-import React from 'react';
-import { YMInitializer } from 'react-yandex-metrika'; // Импорт YMInitializer
+import React, { useEffect } from 'react';
+import { YMInitializer } from 'react-yandex-metrika';
+import ReactPixel from 'react-facebook-pixel';
 
 function App() {
+  useEffect(() => {
+    // Advanced Matching
+    const advancedMatching = {}; 
+    // Опции для конфигурации пикселя
+    const options = {
+      autoConfig: true,  // set to true if you want automatic configuration
+      debug: false,      // enable logs for debugging
+    };
+    // Инициализация Facebook Pixel
+    ReactPixel.init('2211111362560772', advancedMatching, options);
+    ReactPixel.pageView(); // Отправить событие просмотра страницы
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Menu />
       <Main />
       <Footer />
-      {/* Инициализация Яндекс.Метрики с вашим ID счетчика */}
       <YMInitializer accounts={[97170920]} options={{ webvisor: true }} />
     </div>
   );
